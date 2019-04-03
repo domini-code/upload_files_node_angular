8b097eb7042d3a2cd966c4b13a7232f566e04bca
Bezael <bezael.perez.amaris@axa.com>
2019-04-03 16:11:02 +0200

    Add cors

--- back-file-upload/server.js
+++ back-file-upload/server.js
@@ -1,21 +1,24 @@
 const express = require('express');  
+const cors = require('cors');
+const app = express();  
+
+const PORT = 3000;
+
+app.use(cors());
+
 const bodyParser = require('body-parser');  
 const multipart = require('connect-multiparty');  
-
-const app = express();  
-const PORT = 3000;
 
 const multipartMiddleware = multipart({  
     uploadDir: './uploads'
 });
 
 app.use(bodyParser.json());  
-
 app.use(bodyParser.urlencoded({  
     extended: true
 }));
 
-app.post('/api/upload', multipartMiddleware, (req, res) => {  
+app.post('/api/upload', multipartMiddleware, (req, res, next) => {  
     res.json({
         'message': 'File uploaded succesfully.'
     });
